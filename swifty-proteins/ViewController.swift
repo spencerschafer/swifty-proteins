f117b3dfcb758f753566bb53e33dff5539dedf9e
spencerschafer <spencerschafer@gmail.com>
2019-01-22 13:01:07 +0200

    Add app icons for Launch Screen

---
+++ swifty-proteins/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swifty-proteins
+//
+//  Created by Spencer SCHAFER on 2019/01/22.
+//  Copyright © 2019 Spencer SCHAFER. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
