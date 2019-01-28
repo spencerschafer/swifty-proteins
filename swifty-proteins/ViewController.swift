97d19b892d440cb10460a3179f74295498a55b8a
spencerschafer <spencerschafer@gmail.com>
2019-01-28 15:42:28 +0200

    Add login screen

--- swifty-proteins/ViewController.swift
+++ swifty-proteins/ViewController.swift
@@ -10,21 +10,26 @@ import UIKit
 import LocalAuthentication
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
         print("[View Controller End]")
         authenticateLogin()
     }
-
+    
     func authenticateLogin() {
-        let context = LAContext()
         print("[Start of Authentication]")
+        let context:LAContext = LAContext()
+        
+        let auth = LAContext().canEvaluatePolicy(LAPolicy.deviceOwnerAuthenticationWithBiometrics, error: nil)
+        print(auth)
+        
         //Checking if device can be authenticated using TouchID
-        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: nil) {
-            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: "HELLOWORLD", reply: { (success, fail) in
-                if (success) {
+        if context.canEvaluatePolicy(LAPolicy.deviceOwnerAuthenticationWithBiometrics, error: nil) {
+            context.evaluatePolicy(LAPolicy.deviceOwnerAuthenticationWithBiometrics, localizedReason: "Login Using TouchID", reply: { (wasSuccess, error ) in
+                if wasSuccess {
                     print("[Authentication Success]")
                 } else {
                     print("[Authentication Failed]")
